1f6599b68d0bde20bf00da21f0f839f398ef3a9f
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-21 16:56:26 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,29 +1,57 @@
-import streamlit as st
-import openai
+from flask import Flask, render_template, request
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
 
-# Set your OpenAI API key here
-openai.api_key = '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'
-def chat_with_bot(prompt):
-    response = openai.Completion.create(
-        engine="davinci",  # Use the 'davinci' engine for the best performance
-        prompt=prompt,
-        max_tokens=50  # Limit the response length
-    )
-    return response.choices[0].text.strip()
+# Create a Flask app instance
+app = Flask(__name__)
 
-def main():
-    st.title("Chatbot with Streamlit")
-    st.write("Chat with the bot:")
+# Your dataset of instructions and diagram elements
+dataset = {
+    # ... instructions and diagram elements ...
+}
+
+# Download necessary NLTK data
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
+nltk.download("wordnet")
+
+# Initialize the WordNet Lemmatizer
+lemmatizer = WordNetLemmatizer()
+
+# Preprocess user input
+def preprocess_input(user_input):
+    # ... pre-processing code ...
+    return preprocessed_tokens
+
+# Generate a system diagram based on instruction
+def generate_flowchart(instruction):
+    # ... flowchart generation code ...
+    return diagram_result
+
+# Define the chat route
+@app.route("/", methods=["GET", "POST"])
+def chat():
+    response = None
     
-    user_input = st.text_input("You:")
-    
-    if user_input:
-        if user_input.lower() == "exit":
-            st.write("Chatbot: Goodbye!")
-        else:
-            prompt = f"You: {user_input}\nChatbot:"
-            response = chat_with_bot(prompt)
-            st.write("Chatbot:", response)
+    # Handle POST requests
+    if request.method == "POST":
+        # Retrieve user input from the form
+        user_input = request.form["user_input"]
+        
+        # Preprocess the user input
+        preprocessed_input = preprocess_input(user_input)
+        
+        # Convert preprocessed input to a coherent instruction
+        instruction = " ".join(preprocessed_input)
+        
+        # Generate a response based on the instruction
+        response = generate_flowchart(instruction)
+        
+    # Render the HTML template and provide the response to display
+    return render_template("index.html", response=response)
 
+# Run the Flask app
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
