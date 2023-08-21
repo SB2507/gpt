9e606dc05da088a83f8df4996b2ccf2aa8bb497d
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-21 18:29:47 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,20 +1,22 @@
 import streamlit as st
-import fitz  # PyMuPDF
-import openai
+import PyPDF2
 
+# Replace the OpenAI API key with your actual API key
 openai.api_key = "YOUR_OPENAI_API_KEY"
 
 def extract_text_from_pdf(pdf_path):
-    doc = fitz.open(pdf_path)
-    text = ""
-    for page in doc:
-        text += page.get_text("text")
-    return text
+    pdf_text = ""
+    with open(pdf_path, "rb") as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        for page_num in range(len(pdf_reader.pages)):
+            pdf_text += pdf_reader.pages[page_num].extract_text()
+    return pdf_text
 
-def chat_with_bot(user_input):
+def chat_with_bot(pdf_text, user_input):
+    full_input = f"PDF Content: {pdf_text}\nUser Input: {user_input}\nBot:"
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=user_input,
+        prompt=full_input,
         max_tokens=50
     )
     return response.choices[0].text.strip()
@@ -29,8 +31,7 @@ def main():
         user_input = st.text_input("Ask a question or provide a prompt:")
         if st.button("Chat with Bot"):
             pdf_text = extract_text_from_pdf(pdf_path)
-            full_input = f"PDF Content: {pdf_text}\nUser Input: {user_input}\nBot:"
-            bot_response = chat_with_bot(full_input)
+            bot_response = chat_with_bot(pdf_text, user_input)
             st.write("Bot Response:", bot_response)
 
 if __name__ == "__main__":
