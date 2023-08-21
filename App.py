a2062466533b17cc4726a935ee322b31cfc09dae
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-21 16:44:04 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,48 +1,29 @@
 import streamlit as st
 import openai
-import pdfplumber
 
 # Set your OpenAI API key here
 openai.api_key = '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'
-
-# Extract text from a PDF in chunks
-def extract_text_in_chunks(pdf_path, chunk_size=10000):
-    text = ""
-    with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text()
-            if len(text) >= chunk_size:
-                yield text
-                text = ""
-        if text:
-            yield text
-
-# Generate a response using GPT-3
-def generate_gpt3_response(prompt):
+def chat_with_bot(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="davinci",  # Use the 'davinci' engine for the best performance
         prompt=prompt,
-        max_tokens=50  # Adjust as needed
+        max_tokens=50  # Limit the response length
     )
     return response.choices[0].text.strip()
 
-# Main Streamlit app
 def main():
-    st.title("PDF Chatbot with GPT-3")
-
-    uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-    if uploaded_file is not None:
-        pdf_path = uploaded_file.name
-        text_chunks = extract_text_in_chunks(pdf_path)
-
-        st.write("Ask a question:")
-        user_question = st.text_input("")
-
-        if st.button("Ask"):
-            if user_question:
-                gpt3_prompt = f"Q: {user_question}\nA:"
-                gpt3_response = generate_gpt3_response(gpt3_prompt)
-                st.write("GPT-3 Response:", gpt3_response)
+    st.title("Chatbot with Streamlit")
+    st.write("Chat with the bot:")
+    
+    user_input = st.text_input("You:")
+    
+    if user_input:
+        if user_input.lower() == "exit":
+            st.write("Chatbot: Goodbye!")
+        else:
+            prompt = f"You: {user_input}\nChatbot:"
+            response = chat_with_bot(prompt)
+            st.write("Chatbot:", response)
 
 if __name__ == "__main__":
     main()
