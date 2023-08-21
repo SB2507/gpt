c7ca547977004dc1354e3a549841dd3193a9485d
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-21 13:21:53 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,14 +1,23 @@
 import streamlit as st
-import openai
-from PyPDF2 import PdfFileReader
-from transformers import GPT3Tokenizer, GPT3LMHeadModel
+import requests
+import pdfplumber
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from docx import Document
+from PyPDF2 import PdfFileReade
 
 # Set your OpenAI API key here
 openai.api_key = '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'
 
 # Load GPT-3 model and tokenizer
-tokenizer = GPT3Tokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
-model = GPT3LMHeadModel.from_pretrained("EleutherAI/gpt-neo-2.7B")
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neo-2.7B")
+
+def fetch_text_from_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
 
 def extract_text_from_pdf(pdf_file):
     pdf_text = ""
@@ -18,13 +27,28 @@ def extract_text_from_pdf(pdf_file):
         pdf_text += page.extractText()
     return pdf_text
 
+def extract_text_from_docx(docx_file):
+    doc = Document(docx_file)
+    docx_text = "\n".join(para.text for para in doc.paragraphs)
+    return docx_text
+
 st.title("Document AI Assistant")
 
-uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+source_option = st.radio("Select Source:", ("URL", "Upload PDF", "Upload Word"))
 
-if uploaded_file is not None:
-    doc_text = extract_text_from_pdf(uploaded_file)
+if source_option == "URL":
+    url = st.text_input("Enter the URL of the document:")
+    if url:
+        doc_text = fetch_text_from_url(url)
+else:
+    uploaded_file = st.file_uploader("Upload a file", type=["pdf", "docx"])
 
+    if source_option == "Upload PDF" and uploaded_file is not None:
+        doc_text = extract_text_from_pdf(uploaded_file)
+    elif source_option == "Upload Word" and uploaded_file is not None:
+        doc_text = extract_text_from_docx(uploaded_file)
+
+if "doc_text" in locals():
     st.text("Document text:")
     st.write(doc_text)
 
