9423fe6cbe165f1cf4a90ded4bed0ed60feee2a0
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-21 17:06:40 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template, request
+import streamlit as st
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
-
-# Create a Flask app instance
-app = Flask(__name__)
 
 # Your dataset of instructions and diagram elements
 dataset = {
@@ -30,28 +27,20 @@ def generate_flowchart(instruction):
     # ... flowchart generation code ...
     return diagram_result
 
-# Define the chat route
-@app.route("/", methods=["GET", "POST"])
-def chat():
-    response = None
+def main():
+    st.title("Chatbot with Streamlit")
+    st.write("Chat with the bot:")
     
-    # Handle POST requests
-    if request.method == "POST":
-        # Retrieve user input from the form
-        user_input = request.form["user_input"]
-        
-        # Preprocess the user input
-        preprocessed_input = preprocess_input(user_input)
-        
-        # Convert preprocessed input to a coherent instruction
-        instruction = " ".join(preprocessed_input)
-        
-        # Generate a response based on the instruction
-        response = generate_flowchart(instruction)
-        
-    # Render the HTML template and provide the response to display
-    return render_template("index.html", response=response)
+    user_input = st.text_input("You:")
+    
+    if st.button("Send"):
+        if user_input.lower() == "exit":
+            st.write("Chatbot: Goodbye!")
+        else:
+            preprocessed_input = preprocess_input(user_input)
+            instruction = " ".join(preprocessed_input)
+            response = generate_flowchart(instruction)
+            st.write("Chatbot:", response)
 
-# Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
