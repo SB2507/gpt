574f122de3b64d8ee74cb0de10c34187f9edd665
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-21 15:13:18 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
- 
 import streamlit as st
 import openai
+import pdfplumber
 
 # Set your OpenAI API key here
 openai.api_key = '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'
@@ -30,20 +30,19 @@ def generate_gpt3_response(prompt):
 def main():
     st.title("PDF Chatbot with GPT-3")
 
-    pdf_path = 'path_to_your_pdf.pdf'
-    text_chunks = extract_text_in_chunks(pdf_path)
+    uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+    if uploaded_file is not None:
+        pdf_path = uploaded_file.name
+        text_chunks = extract_text_in_chunks(pdf_path)
 
-    st.write("Ask a question:")
-    user_question = st.text_input("")
+        st.write("Ask a question:")
+        user_question = st.text_input("")
 
-    if st.button("Ask"):
-        if user_question:
-            answer = search_for_answer(text_chunks, user_question)
-            st.write("Keyword-based Answer:", answer)
-
-            gpt3_prompt = f"Q: {user_question}\nA: {answer}\nQ:"
-            gpt3_response = generate_gpt3_response(gpt3_prompt)
-            st.write("GPT-3 Response:", gpt3_response)
+        if st.button("Ask"):
+            if user_question:
+                gpt3_prompt = f"Q: {user_question}\nA:"
+                gpt3_response = generate_gpt3_response(gpt3_prompt)
+                st.write("GPT-3 Response:", gpt3_response)
 
 if __name__ == "__main__":
     main()
