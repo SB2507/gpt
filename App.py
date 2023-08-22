a1c9ba04c93b0ecfc14a05a5d9a741c5ec1deac8
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-22 11:13:40 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,27 +2,51 @@
 # Replace "YOUR_OPENAI_API_KEY" with your actual OpenAI API key
 #openai.api_key = "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"
 
+from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
 
-def extract_text_from_pdf(pdf_path):
-    reader = PdfReader(pdf_path)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
+
 
 def main():
-    st.title("PDF Text Extraction")
+    load_dotenv()
+    st.set_page_config(page_title="Chat PDF")
+    st.header("Chat PDF 💬")
+    
+    # upload file
+    pdf = st.file_uploader("Upload your PDF file", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      # split into chunks
+      char_text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000,
+                                                 chunk_overlap=200,length_function=len)
+      text_chunks = char_text_splitter.split_text(text)
+      
+      # create embeddings
+      embeddings = OpenAIEmbeddings()
+      docsearch = FAISS.from_texts(text_chunks, embeddings) 
+      llm = OpenAI() 
+      chain = load_qa_chain(llm, chain_type="stuff")
+      
+      # show user input
+      query = st.text_input("Type your question:")
+      if query:
+        docs = docsearch.similarity_search(query)
+        response = chain.run(input_documents=docs, question=query)
+           
+        st.write(response)
+    
 
-    pdf_path = st.file_uploader("Upload PDF File", type=["pdf"])
-
-    if pdf_path:
-        st.write("PDF uploaded successfully.")
-        pdf_text = extract_text_from_pdf(pdf_path)
-        st.subheader("Extracted Text:")
-        st.text(pdf_text)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
