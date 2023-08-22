d785f4aac57637a641d6a4e2e528eb2ced921a36
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-22 11:43:58 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,8 +1,5 @@
-
-# Replace "YOUR_OPENAI_API_KEY" with your actual OpenAI API key
-#openai.api_key = "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"
-
 from dotenv import load_dotenv
+import os  # Import the os module
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
@@ -11,10 +8,10 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 
-
+# Load environment variables from .env file
+load_dotenv()
 
 def main():
-    load_dotenv()
     st.set_page_config(page_title="Chat PDF")
     st.header("Chat PDF 💬")
     
@@ -23,29 +20,32 @@ def main():
     
     # extract the text
     if pdf is not None:
-      pdf_reader = PdfReader(pdf)
-      text = ""
-      for page in pdf_reader.pages:
-        text += page.extract_text()
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
         
-      # split into chunks
-      char_text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000,
-                                                 chunk_overlap=200,length_function=len)
-      text_chunks = char_text_splitter.split_text(text)
-      
-      # create embeddings
-      embeddings = OpenAIEmbeddings()
-      docsearch = FAISS.from_texts(text_chunks, embeddings) 
-      llm = OpenAI() 
-      chain = load_qa_chain(llm, chain_type="stuff")
-      
-      # show user input
-      query = st.text_input("Type your question:")
-      if query:
-        docs = docsearch.similarity_search(query)
-        response = chain.run(input_documents=docs, question=query)
+        # split into chunks
+        char_text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000,
+                                                   chunk_overlap=200,length_function=len)
+        text_chunks = char_text_splitter.split_text(text)
+        
+        # Access OpenAI API key from environment variable
+        openai_api_key = os.getenv("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")
+        
+        # create embeddings
+        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)  # Pass the API key here
+        docsearch = FAISS.from_texts(text_chunks, embeddings) 
+        llm = OpenAI() 
+        chain = load_qa_chain(llm, chain_type="stuff")
+        
+        # show user input
+        query = st.text_input("Type your question:")
+        if query:
+            docs = docsearch.similarity_search(query)
+            response = chain.run(input_documents=docs, question=query)
            
-        st.write(response)
+            st.write(response)
     
 
 if __name__ == '__main__':
