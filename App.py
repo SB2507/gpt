d1f9fceb660b716f9f02dbbad9006fb76480c519
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-22 12:16:17 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,52 +1,68 @@
-from dotenv import load_dotenv
-import os  # Import the os module
 import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+import fitz  # PyMuPDF for PDF processing
+import faiss  # FAISS for vector similarity search
+import openai  # OpenAI for embeddings
+from dotenv import load_dotenv
+import os
 
 # Load environment variables from .env file
 load_dotenv()
 
+# Main function
 def main():
+    # Set Streamlit page configuration
     st.set_page_config(page_title="Chat PDF")
     st.header("Chat PDF 💬")
     
-    # upload file
+    # Upload PDF file
     pdf = st.file_uploader("Upload your PDF file", type="pdf")
     
-    # extract the text
+    # Initialize OpenAI API
+    openai.api_key =("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")
+    
+    # Initialize FAISS index
+    index = faiss.IndexFlatL2(768)  # Assuming 768-dimensional embeddings
+    
+    # If a PDF is uploaded
     if pdf is not None:
-        pdf_reader = PdfReader(pdf)
+        # Extract text from PDF pages
+        pdf_document = fitz.open(pdf)
         text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+        for page_num in range(pdf_document.page_count):
+            page = pdf_document.load_page(page_num)
+            text += page.get_text("text")
         
-        # split into chunks
-        char_text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000,
-                                                   chunk_overlap=200,length_function=len)
-        text_chunks = char_text_splitter.split_text(text)
+        # Split text into chunks
+        chunk_size = 1000
+        text_chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
         
-        # Access OpenAI API key from environment variable
-        openai_api_key =("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")
+        # Create embeddings for text chunks
+        embeddings = []
+        for chunk in text_chunks:
+            response = openai.Embedding.create(texts=[chunk])
+            embeddings.append(response['data'][0]['vector'])
+        embeddings = faiss.normalize_L2(embeddings)
         
-        # create embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)  # Pass the API key here
-        docsearch = FAISS.from_texts(text_chunks, embeddings) 
-        llm = OpenAI() 
-        chain = load_qa_chain(llm, chain_type="stuff")
+        # Add embeddings to FAISS index
+        index.add(embeddings)
         
-        # show user input
+        # Get user input question
         query = st.text_input("Type your question:")
         if query:
-            docs = docsearch.similarity_search(query)
-            response = chain.run(input_documents=docs, question=query)
-           
-            st.write(response)
-    
+            # Embed the query
+            query_embedding = openai.Embedding.create(texts=[query])['data'][0]['vector']
+            query_embedding = faiss.normalize_L2(query_embedding)
+            
+            # Perform similarity search
+            num_results = 5
+            distances, indices = index.search(query_embedding, num_results)
+            
+            # Display search results
+            for i in range(num_results):
+                st.write(f"Result {i+1}: Similarity Score {1 - distances[0][i]:.2f}")
+                st.write(text_chunks[indices[0][i]])
+                st.write("-" * 30)
 
+# Run the main function when the script is executed
 if __name__ == '__main__':
     main()
