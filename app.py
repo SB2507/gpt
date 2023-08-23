703ff9afe846f4f02b66d2e34023f4fc2788a88e
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-23 15:41:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
-from flask import Flask, render_template, request
-import pdfquery
-from lxml import etree
+import streamlit as st
+import pytesseract
+from PIL import Image
+import fitz  # PyMuPDF
 
-app = Flask(__name__)
+# Set the title of the Streamlit app
+st.title("Text Extraction App")
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    customer_name = ""
+def ocr_text(text):
+    try:
+        extracted_text = text
+        return extracted_text
+    except Exception as e:
+        return str(e)
 
-    if request.method == 'POST':
-        pdf = pdfquery.PDFQuery(request.files['pdf'])
-        pdf.load()
+# Streamlit UI components
+st.sidebar.header("Options")
+file_type = st.sidebar.selectbox("Select File Type", ["PDF"])
 
-        # Convert the PDF to XML
-        xml_tree = pdf.tree
-        xml_text = etree.tostring(xml_tree, pretty_print=True, encoding='unicode')
+if file_type == "PDF":
+    uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+    if uploaded_file is not None:
+        if st.button("Extract and Perform OCR"):
+            # Convert PDF to text using PyMuPDF
+            pdf_document = fitz.open(uploaded_file)
+            num_pages = pdf_document.page_count
+            extracted_text = ""
+            for page_num in range(num_pages):
+                page = pdf_document[page_num]
+                extracted_text += page.get_text()
+            pdf_document.close()
 
-        # Access the data using coordinates
-        coordinates = [68.0, 231.57, 101.990, 234.893]
-        customer_name = pdf.tree.find('LTTextLineHorizontal:in_bbox("%s, %s, %s, %s")' % tuple(coordinates)).text
-
-    return render_template('index.html', customer_name=customer_name)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+            # Perform OCR on extracted text
+            extracted_text = ocr_text(extracted_text)
+            
+            st.subheader("Extracted Text:")
+            st.write(extracted_text)
