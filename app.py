f5d5075d93c2ffa95822c0161626f30f0370b0c9
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-23 15:54:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,23 +16,20 @@ def ocr_text(text):
 # Streamlit UI components
 st.sidebar.header("Options")
 file_type = st.sidebar.selectbox("Select File Type", ["PDF"])
-
-if file_type == "PDF":
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-    if uploaded_file is not None:
-        if st.button("Extract and Perform OCR"):
-            # Convert PDF to text using PyMuPDF
-            pdf_bytes = uploaded_file.read()  # Read the uploaded file as bytes
-            pdf_document = fitz.open(pdf="pdf", stream=pdf_bytes)
-            num_pages = pdf_document.page_count
-            extracted_text = ""
-            for page_num in range(num_pages):
-                page = pdf_document[page_num]
-                extracted_text += page.get_text()
-            pdf_document.close()
+    st.button("Extract and Perform OCR"):
+    # Convert PDF to text using PyMuPDF
+     pdf_bytes = uploaded_file.read()  # Read the uploaded file as bytes
+     pdf_document = fitz.open(pdf="pdf", stream=pdf_bytes)
+     num_pages = pdf_document.page_count
+    extracted_text = ""
+    for page_num in range(num_pages):
+        page = pdf_document[page_num]
+        extracted_text += page.get_text()
+        pdf_document.close()
 
             # Perform OCR on extracted text
-            extracted_text = ocr_text(extracted_text)
-            
-            st.subheader("Extracted Text:")
-            st.write(extracted_text)
+    extracted_text = ocr_text(extracted_text)
+        
+    st.subheader("Extracted Text:")
+    st.write(extracted_text)
