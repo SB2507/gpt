41de3758e564752c7482c13a57eabb5d30d9ef70
Suraj Bhute <62201211+SB2507@users.noreply.github.com>
2023-08-23 16:04:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,27 +18,38 @@ def ocr_text(text):
 # Streamlit UI components
 st.sidebar.header("Options")
 file_type = st.sidebar.selectbox("Select File Type", ["PDF"])
-uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-if uploaded_file is not None:
-    if st.button("Extract and Perform OCR"):
+
+if file_type == "PDF":
+    uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+    if uploaded_file is not None:
+        if st.button("Extract and Perform OCR"):
             # Convert PDF to text using PyMuPDF
-        with tempfile.NamedTemporaryFile(delete=False) as temp_pdf:
-            temp_pdf.write(uploaded_file.read())
-            temp_pdf_path = temp_pdf.name
+            with tempfile.NamedTemporaryFile(delete=False) as temp_pdf:
+                temp_pdf.write(uploaded_file.read())
+                temp_pdf_path = temp_pdf.name
             
-        pdf_document = fitz.open(temp_pdf_path)
-        num_pages = pdf_document.page_count
-        extracted_text = ""
-        for page_num in range(num_pages):
-            page = pdf_document[page_num]
-            extracted_text += page.get_text()
-        pdf_document.close()
+            pdf_document = fitz.open(temp_pdf_path)
+            num_pages = pdf_document.page_count
+            extracted_text = ""
+            for page_num in range(num_pages):
+                page = pdf_document[page_num]
+                extracted_text += page.get_text()
+            pdf_document.close()
 
             # Perform OCR on extracted text
-        extracted_text = ocr_text(extracted_text)
+            extracted_text = ocr_text(extracted_text)
             
-        st.subheader("Extracted Text:")
-        st.write(ocr_text(extracted_text))
+            st.subheader("Extracted Text:")
+            st.write(extracted_text)
+            
+            # Provide a download button for the extracted text
+            with st.beta_expander("Download Extracted Text"):
+                st.download_button(
+                    label="Download Text",
+                    data=extracted_text.encode("utf-8"),
+                    file_name="extracted_text.txt",
+                    mime="text/plain"
+                )
             
             # Clean up the temporary PDF file
-        os.remove(temp_pdf_path)
+            os.remove(temp_pdf_path)
